570666123f6d6339318c5d9d4d718a819b4ca41e
Angel-senpai <daniil.murygin68@gmail.com>
2020-11-30 17:47:09 +0300

    Урок 2. Архитектура кода

---
+++ GBShop/App/Entity/LoginResult.swift
@@ -0,0 +1,13 @@
+//
+//  LoginResult.swift
+//  GBShop
+//
+//  Created by Даниил Мурыгин on 30.11.2020.
+//
+
+import Foundation
+
+struct LoginResult: Codable {
+    let result: Int
+    let user: User
+}
